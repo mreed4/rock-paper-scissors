cd3b9eb71eb7115efbca8ff07ac938da52bc9c31
Matthew <matthew.reed@outlook.com>
2021-07-18 17:43:18 -0700

    Function for computer choice

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
-const computerPLay = (playerSelection, computerSelection) => {
-    
+const computerPlay = () => {
+
+
+    let n = Math.floor(Math.random() * 3);
+    // console.log(compHand);
+
+    switch (n) {
+        case 0:
+            // console.log(`Rock`);
+            return `Rock`;
+            break;
+        case 1:
+            // console.log(`Paper`);
+            return `Paper`;
+            break;
+        case 2:
+            // console.log(`Scissors`);
+            return `Scissors`;
+            break;
+        default:
+            return `Oopsie!`;
+    }
 }
+
+/*
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+*/
+
