3cc37f970a074aa6a1744c15177b1f20bbcb121d
Matthew <matthew.reed@outlook.com>
2021-07-18 18:15:55 -0700

    Begin logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,47 @@
 const computerPlay = () => {
 
-
     let n = Math.floor(Math.random() * 3);
     // console.log(compHand);
 
     switch (n) {
         case 0:
-            // console.log(`Rock`);
-            return `Rock`;
+            console.log(`rock`);
+            return `rock`;
             break;
         case 1:
-            // console.log(`Paper`);
-            return `Paper`;
+            console.log(`paper`);
+            return `paper`;
             break;
         case 2:
-            // console.log(`Scissors`);
-            return `Scissors`;
+            console.log(`scissors`);
+            return `scissors`;
             break;
         default:
             return `Oopsie!`;
     }
 }
 
-/*
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-*/
 
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+
+
+
+const playRound = (playerSelection, computerSelection) => {
+
+    playerSelection = `rOCk`.toLowerCase();
+    // console.log(playerSelection);
+
+    computerSelection = computerPlay();
+
+    if (computerSelection === playerSelection) {
+        console.log(`Tie!`);
+    } else {
+        console.log(`Placeholder`);
+    }
+}
+
+playRound();
